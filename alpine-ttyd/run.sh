2994029ad3408addc82fb3b23fb4ad2652a0b853
darryl west <dpw@raincitysoftware.com>
2022-02-25 11:43:30 -0800

    ttyd

---
+++ alpine-ttyd/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# dpw@Darryls-iMac.localdomain
+# 2022-02-25 19:29:46
+#
+
+set -eu
+
+export DOCKER_REPO=darrylwest
+export IMAGE=${DOCKER_REPO}/alpine-ttyd
+export VSN=$(cat version)
+export name=alpine-ttyd
+
+# no volumes, all source is in the container
+docker run --name $name \
+    -it \
+    --detach \
+    -p 7681:7681 \
+    "$IMAGE:$VSN"
+
+sleep 2
+docker ps | fgrep $name
+
